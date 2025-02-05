8da3e00020b963acf29271b892de38585badbe0a
pedrohmtoledo <toledopedro.hmt@gmail.com>
2025-02-05 17:56:52 +0000

    template

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-import "./styles/static.css"
-import "./styles/cards.css"
-import taskIMg from "./img/task.svg"
-import Task from "./modules/todos.js"
-import Projects from "./modules/project.js"
-import renderStatic from "./modules/static.js"
-import "./styles/dialog.css"
-import { openTaskDialog, submitTask } from "./modules/task.js"
-import renderDialog from "./modules/dialog.js"
-import dom from "./modules/dom.js"
+import './styles/static.css';
+import { hello } from './modules/javascript';
 
-
-
-dom();
+console.log('hello');
